d3483f9f4e97b28d1e1ff81db0a36e0f0f916e84
jr-b <j@jrb.nz>
2022-03-15 18:17:00 -0400

    firt commit-scrape iciradiocanada.ca for learning

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# scrape-hacker-news-by-domain
-
-Scrape HN to track links from specific domains
-
-See [Scraping web pages from the command-line with shot-scraper](https://simonwillison.net/2022/Mar/14/scraping-web-pages-shot-scraper/) for details of how this works.
+# Project for learning some git scraping!
--- scrape.js
+++ scrape.js
@@ -1,18 +1,11 @@
-Array.from(document.querySelectorAll('.itemlist .athing')).map(el => {
-  const title = el.querySelector('a.titlelink').innerText;
-  const points = parseInt(el.nextSibling.querySelector('.score').innerText);
-  const url = el.querySelector('a.titlelink').href;
-  const dt = el.nextSibling.querySelector('.age').title;
-  const submitter = el.nextSibling.querySelector('.hnuser').innerText;
-  const commentsUrl = el.nextSibling.querySelector('.age a').href;
-  const id = commentsUrl.split('?id=')[1];
-  // Only posts with comments have a comments link
-  const commentsLink = Array.from(
-    el.nextSibling.querySelectorAll('a')
-  ).filter(el => el && el.innerText.includes('comment'))[0];
-  let numComments = 0;
-  if (commentsLink) {
-    numComments = parseInt(commentsLink.innerText.split()[0]);
-  }
-  return {id, title, url, dt, points, submitter, commentsUrl, numComments};
+Array.from(document.querySelectorAll('.first-lineup .nb-item--7 li:not(.related-item)')).map(el => {
+  const title = el.querySelector('a.title-link').innerText;
+  // const points = parseInt(el.nextSibling.querySelector('.score').innerText);
+  // const url = el.querySelector('a.titlelink').href;
+  // const dt = el.nextSibling.querySelector('.age').title;
+  // const submitter = el.nextSibling.querySelector('.hnuser').innerText;
+  // const commentsUrl = el.nextSibling.querySelector('.age a').href;
+  // const id = commentsUrl.split('?id=')[1];
+
+  return {title};
 })
