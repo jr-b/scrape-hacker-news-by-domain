1c57e8d68472f615c0b260bcb982c4bcf4291e9b
jr-b <j@jrb.nz>
2022-08-29 11:46:28 -0400

    change to url-now scraping radiocanada mediasnumeriques job page

--- scrape.js
+++ scrape.js
@@ -1,5 +1,7 @@
-Array.from(document.querySelectorAll('a.card')).map(el => {
-  const title = el.querySelector('.card-content').innerText;
+Array.from(document.querySelectorAll('article')).map(el => {
+  const title = el.querySelector('h1.text-huge-from-xs').innerText;
+  const info = el.querySelector('div.line-height-normal').innerText;
+  const descrip = el.querySelector('div.text-current-to-xs').innerText;
   // const points = parseInt(el.nextSibling.querySelector('.score').innerText);
   // const url = el.querySelector('a.titlelink').href;
   // const dt = el.nextSibling.querySelector('.age').title;
@@ -7,5 +9,5 @@ Array.from(document.querySelectorAll('a.card')).map(el => {
   // const commentsUrl = el.nextSibling.querySelector('.age a').href;
   // const id = commentsUrl.split('?id=')[1];
 
-  return {title};
+  return {title, info, descrip};
 })
